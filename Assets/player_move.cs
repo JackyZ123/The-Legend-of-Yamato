003ead82232848d166c23bfa3a8b8088237e202c
JasonLi13 <83623008+JasonLi13@users.noreply.github.com>
2022-05-17 09:44:40 +1200

    player movement

---
+++ Assets/player_move.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_move : MonoBehaviour
+{
+    Rigidbody2D body;
+
+    float horzontal;
+    float vertical;
+
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
