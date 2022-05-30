a022b776cc5cab5f5b30873613bfa2a622c86d08
JackyZ123 <jackyzhang1501@gmail.com>
2022-05-31 09:56:17 +1200

    not good

--- Assets/Player/Scripts/PlayerMove.cs
+++ Assets/Player/Scripts/PlayerMove.cs
@@ -10,13 +10,6 @@ public class PlayerMove : MonoBehaviour
 
     public float speed = 5f;
 
-    [Header("Boost")]
-    public float boost = 2f;
-    public float boost_time;
-    public float boost_multiplier;
-    public float boost_delay = 2f;
-    public float boost_delay_time;
-
     private void Awake()
     {
         if (!motor)
@@ -27,9 +20,6 @@ public class PlayerMove : MonoBehaviour
         {
             levelManager = GameObject.Find("LevelManager").GetComponent<LevelManager>();
         }
-
-        boost_time = boost;
-        boost_delay_time = boost_delay;
     }
 
     private void Start()
@@ -45,31 +35,6 @@ public class PlayerMove : MonoBehaviour
         float vertical_input = Input.GetAxisRaw("Vertical");
         float horizontal_input = Input.GetAxisRaw("Horizontal");
 
-        bool is_boost = Input.GetKey(KeyCode.LeftShift);
-
-        float move_speed = speed;
-
-        if (is_boost && boost_time > 0)
-        {
-            move_speed *= boost_multiplier;
-            boost_time -= Time.deltaTime;
-            boost_delay_time = boost_delay;
-        }
-        else
-        {
-            if (boost_delay_time <= 0)
-            {
-                if (boost_time <= boost)
-                {
-                    boost_time += Time.deltaTime;
-                }
-            }
-            else
-            {
-                boost_delay_time -= Time.deltaTime;
-            }
-        }
-
-        motor.Move(move_speed, new Vector2(horizontal_input, 0).normalized + new Vector2(0, vertical_input).normalized);
+        motor.Move(speed, new Vector2(horizontal_input, 0).normalized + new Vector2(0, vertical_input).normalized);
     }
 }
