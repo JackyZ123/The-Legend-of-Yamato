8ce15957e812852e3cd78de81c428e68bf4d81a4
JasonLi13 <83623008+JasonLi13@users.noreply.github.com>
2022-06-01 20:07:05 +1200

    player health

---
+++ Assets/Player/Scripts/Health.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField]
+    private int health = 5;
+
+    private int Maxhealth = 5;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Damage(1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            Heal(1);
+        }
+    }
+
+    public void Damage(int amount)
+    //damage done to health
+    {
+        if (amount < 0)
+        {
+            Debug.Log("cannot have negative damage");
+        }
+        this.health -= amount;
+        if (health < 0)
+        {
+            Die();
+        }
+    }
+
+    public void Heal(int amount)
+    //healing done to health
+    {
+        if (amount < 0)
+        {
+            Debug.Log("cannot have negative heal");
+        }
+        if (health + amount > Maxhealth)
+        {
+            this.health = Maxhealth;
+        }
+        else
+        {
+            this.health += amount;
+        }
+    }
+
+    private void Die()
+    //if health is 0
+    {
+        Debug.Log("YOU DIED");
+        Destroy(gameObject);
+    }
+}
