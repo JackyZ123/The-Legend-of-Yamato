713d8913bbb35ea1101c257e4377a6426e9d32b5
JackyZ123 <jackyzhang1501@gmail.com>
2022-06-04 18:38:42 +1200

    basic enemy move
    enemy move by distance to player
    
    enemy stops moving at the correct distance

--- Assets/Player/Scripts/Motor.cs
+++ Assets/Player/Scripts/Motor.cs
@@ -6,7 +6,9 @@ using UnityEngine;
 public class Motor : MonoBehaviour
 {
     public Rigidbody2D rb;
-    Vector2 velocity = Vector2.zero;
+    public Vector2 positionToGo;
+
+    public float speed;
 
     private void Awake()
     {
@@ -14,21 +16,46 @@ public class Motor : MonoBehaviour
         {
             rb = GetComponent<Rigidbody2D>();
         }
+
+        positionToGo = transform.position;
     }
 
-    public void Move(float speed, Vector2 direction)
+    public void Move(float newSpeed, Vector2 direction)
     {
         // moves the gameObject based on direction and speed
 
-        velocity = speed * direction;
-        rb.velocity = velocity;
+        speed = newSpeed;
+
+        positionToGo = (speed + 1) * direction + (Vector2)transform.position;
     }
 
-    public void MoveTo(float speed, Vector2 location, float randomness = 0f, bool avoidObstacles = false)
+    public void MoveTo(float newSpeed, Vector2 location, float randomness = 0f, bool avoidObstacles = false)
     {
         // move gameObject to a location
 
         // randomness is how "floaty" it is
         // avoid obstacles make the object move around obstacle
+
+        speed = newSpeed;
+
+        Vector2 scaledDirection = (location - (Vector2)transform.position) * 1.1f;
+
+        positionToGo = scaledDirection + (Vector2)transform.position;
+
+        positionToGo += new Vector2(Random.Range(-randomness, randomness), Random.Range(-randomness, randomness));
+    }
+
+    private void Update()
+    {
+        Vector2 direction = positionToGo - (Vector2)transform.position;
+
+        if (direction.magnitude > 0.1f)
+        {
+            rb.velocity = direction.normalized * speed;
+        }
+        else
+        {
+            rb.velocity = Vector2.zero;
+        }
     }
 }
